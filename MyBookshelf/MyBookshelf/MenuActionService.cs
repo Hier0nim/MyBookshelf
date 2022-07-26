289ac7a9f98b90f07c9e6329c6bea9fc8656b799
Hieronim <hieronimdaniel@gmail.com>
2022-07-26 21:59:55 +0200

    First commit
    Starting the project MyBookshelf

---
+++ MyBookshelf/MyBookshelf/MenuActionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookshelf
+{
+    internal class MenuActionService
+    {
+        private List<MenuAction> menuAction;
+
+        public void AddNewAction(int id, string name, string menuCategory)
+        {
+            MenuAction action = new MenuAction() { Id = id, Name = name, MenuCategory = menuCategory };
+            menuAction.Add(action);
+        }
+
+        public List<MenuAction> GetMenuActionByMenuCategory(string menuCategory)
+        {
+            List<MenuAction> result = new List<MenuAction>();
+
+            return result;
+        }
+    }
+}
---
+++ MyBookshelf/MyBookshelf/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyBookshelf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MyBookshelf app!");
+            Console.WriteLine("What do you want to do?");
+        }
+    }
+}
+
+// Welcomming
+// Action selection
+// 1. Book Management
+//  1(a). Addition of a book
+//      - Selection of book category (more than 1)
+//      - Details
+//  1(b). Deletion of a book
+//      - id or name
+//  1(c). Modification of a book
+//      - zmiana szczegolow
+// 2. Condition of bookshelf
+//  2(a) Everything
+//  2(a) By categories
+//  2(c) Selected category
+//      - selection of category
+// 3. Book has been read
+//      - Selection of existing book.
